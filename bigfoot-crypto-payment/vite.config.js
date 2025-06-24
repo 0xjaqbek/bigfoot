09d8ba53291db37e3ce78cb9a80dbdeadf9d03a9
0xjaqbek <jaqbek.eth@gmail.com>
2025-06-24 04:38:06 +0200

    Fix base path for bigfoot repo

--- bigfoot-crypto-payment/vite.config.js
+++ bigfoot-crypto-payment/vite.config.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bigfoot-crypto-payment/',
+  base: '/bigfoot/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
